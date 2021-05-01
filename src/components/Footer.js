82b29c89fcbec283b6b2d17074dca66e632c66a7
tony <anthonyanselmo92@gmail.com>
2021-04-30 22:05:51 -0700

    adding files

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Let's Fuck Around and Break Shit!
+                </p>
+                <p className="footer-subscription-text">
+                    Created By Tony.
+                </p>
+            </section>
+        </div>
+    );
+};
+
+export default Footer;
