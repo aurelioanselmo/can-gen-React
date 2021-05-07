ea6accc029b978996ad97404891cd0957631638e
Edward M <edwardtmuniz@gmail.com>
2021-05-06 20:47:12 -0700

    Crappy working modal

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import '../../App.css';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+import NewModal from "../NewModal";
 
 export default function Services() {
-    return (
-        <>
-            <div className="container">
-                <button className="lftp-line">Lftp Generator</button>
-                <button className="resources">Resources</button>
-            </div>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <NewModal title={"My Modal"} content={"My Content"} />
+        <button className="lftp-line">Lftp Generator</button>
+        <button className="resources">Resources</button>
+      </div>
+      <Footer />
+    </>
+  );
 }
