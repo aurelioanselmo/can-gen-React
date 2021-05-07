51dcf9c91d40cad6f4ff1d4a4031e6b96da9c912
Edward M <edwardtmuniz@gmail.com>
2021-05-06 20:10:04 -0700

    Working now

--- src/components/pages/Emails.js
+++ src/components/pages/Emails.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import '../../App.css';
-import '../Modal.css';
-import Footer from '../Footer';
-import Modal from '../Modal';
-import Close from '@material-ui/icons/Close';
-import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
-import templates from '../../templates.json';
-
+import React from "react";
+import "../../App.css";
+import "../Modal.css";
+import Footer from "../Footer";
+import Modal from "../Modal";
+// import Close from '@material-ui/icons/Close';
+// import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import templates from "../../templates.json";
 
 function Emails() {
   const [modalIndex, setModalIndex] = React.useState(0);
-  const [body, setBody] = React.useState('');
-  const [title, setTitle] = React.useState('');
+  const [body, setBody] = React.useState("");
+  const [title, setTitle] = React.useState("");
   const [copied, setCopied] = React.useState();
   const modalRef = React.useRef();
 
   const openModal = (i) => {
-    setModalIndex(i)
-    setBody(templates[i].body)
-    setTitle(templates[i].title)
-    modalRef.current.openModal()
+    setModalIndex(i);
+    setBody(templates[i].body);
+    setTitle(templates[i].title);
+    modalRef.current.openModal();
   };
 
   const copyText = () => {
@@ -27,63 +26,75 @@ function Emails() {
     copyText.select();
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
-    setCopied(true)
-    setTimeout(() => setCopied(false), 3000)
-  }
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
 
   const handleVar = (event, variable) => {
-    const regex = new RegExp(`--${variable}--`, 'g');
-    const updatedBody = body.replace(regex, event.target.value)
-    const updatedTitle = title.replace(regex, event.target.value)
-    setBody(updatedBody)
-    setTitle(updatedTitle)
-  }
+    const regex = new RegExp(`--${variable}--`, "g");
+    const updatedBody = body.replace(regex, event.target.value);
+    const updatedTitle = title.replace(regex, event.target.value);
+    setBody(updatedBody);
+    setTitle(updatedTitle);
+  };
 
   const maybeReset = (e) => {
-    if (!e.target.value){return}
-    setBody(templates[modalIndex].body)
-    setTitle(templates[modalIndex].title)
-    console.log(body)
-    const varInputs = document.querySelectorAll('.var-input')
-    varInputs.forEach((e,i) => {
-      varInputs[i].value = ''
-    })
-  }
+    if (!e.target.value) {
+      return;
+    }
+    setBody(templates[modalIndex].body);
+    setTitle(templates[modalIndex].title);
+    console.log(body);
+    const varInputs = document.querySelectorAll(".var-input");
+    varInputs.forEach((e, i) => {
+      varInputs[i].value = "";
+    });
+  };
 
   return (
     <div className={`email-templates`}>
-      { templates.map((e, i) => {
-        return (<div key={`template${i}`}>
-          <button className={templates[modalIndex].cssClass} onClick={() => openModal(i)}>{templates[i].title}</button>
-        </div>)
-      })
-      }
+      {templates.map((e, i) => {
+        return (
+          <div key={`template${i}`}>
+            <button
+              className={templates[modalIndex].cssClass}
+              onClick={() => openModal(i)}
+            >
+              {templates[i].title}
+            </button>
+          </div>
+        );
+      })}
       <Modal ref={modalRef}>
-        {templates[modalIndex].vars && templates[modalIndex].vars.map(each => {
-          return (
-            <input 
-            key={each}
-            className='var-input'
-            placeholder={each}
-            onFocus={maybeReset}
-            onBlur={event => handleVar(event, each)}
-            onKeyDown={event => event.key === 'Enter' && handleVar(event, each)}
-            />
-          )
-        })}
-        <div className='top-head'>
+        {templates[modalIndex].vars &&
+          templates[modalIndex].vars.map((each) => {
+            return (
+              <input
+                key={each}
+                className="var-input"
+                placeholder={each}
+                onFocus={maybeReset}
+                onBlur={(event) => handleVar(event, each)}
+                onKeyDown={(event) =>
+                  event.key === "Enter" && handleVar(event, each)
+                }
+              />
+            );
+          })}
+        <div className="top-head">
           <h1>{title}</h1>
           <button onClick={() => modalRef.current.close()}>
-          <Close></Close>
+            {/* <Close></Close> */}X
           </button>
         </div>
         <p>
-          <textarea id='template' value={body} readOnly></textarea>
+          <textarea id="template" value={body} readOnly></textarea>
         </p>
-        <button className='copy-text' onClick={copyText}>
-          <FileCopyOutlinedIcon />
+        <button className="copy-text" onClick={copyText}>
+          {/* <FileCopyOutlinedIcon /> */}
+          COPY
         </button>
-        {copied && <span id='copied'>copied text</span>}
+        {copied && <span id="copied">copied text</span>}
       </Modal>
       <>
         <Footer />
